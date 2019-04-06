a797d1665dfa3fd0d7ede7296899b91c4564ed37
Tom <tcantinelli@gmail.com>
2019-04-06 03:31:26 +0200

    Routes 190405

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 const AuthentificationController = require('../controllers/authentification');
+const CategoryController = require('../controllers/category');
+const PlaceController = require('../controllers/place');
+const SessionController = require('../controllers/session');
+
 require('../services/passport');
 const passport = require('passport');
 
@@ -9,9 +13,21 @@ module.exports = server => {
 	//Authentification
 	server.post('/signup', AuthentificationController.signup);
 	server.post('/signin', requireValidCredentials, AuthentificationController.signin);
-	//Projects
+	
 	// server.get('/projects', ProjectController.readAll);
 	// server.get('/project/:id', ProjectController.read);
 	// server.post('/project', ProjectController.create);
 	// server.delete('/project/:id', ProjectController.delete);
+
+	//Category
+	server.post('/cat', CategoryController.create);
+	server.get('/cats', CategoryController.readAll);
+
+	//Category
+	server.post('/place', PlaceController.create);
+	server.get('/places', PlaceController.readAll);
+
+	//Session
+	server.post('/session', SessionController.create);
+	server.get('/sessions', SessionController.readAll);
 };
