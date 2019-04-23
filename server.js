2568a4e8dc6e61cf1893b892cde5de5a8a508540
Thomas Cantinelli <tcantinelli@gmail.com>
2019-04-23 22:10:34 +0200

    minor

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes/index');
+const routes = require('./routes');
 const bodyParser = require('body-parser');
 const server = express();
 const cors = require('cors');
