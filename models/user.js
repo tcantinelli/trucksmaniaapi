d788f1b9b780c8811622201111ab9550cd2f6af2
Tom <tcantinelli@gmail.com>
2019-05-05 01:45:13 +0200

    delete date User

--- models/user.js
+++ models/user.js
@@ -5,7 +5,6 @@ const bcrypt = require('bcrypt-nodejs');
 const UserSchema = new Schema({
 	email: String,
 	password: String,
-	inscription: Date,
 	account: String, //"foodtruck" ou "client"
 	validation: { type: Boolean, default: false }, //Validation inscription par email
 	//Favoris pour clients, FT pour FT à l'index 0 du tableau
