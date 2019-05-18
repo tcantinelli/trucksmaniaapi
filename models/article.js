fea40956d10aec36968aef5318aaa255970ec946
Thomas Cantinelli <tcantinelli@gmail.com>
2019-05-19 01:52:22 +0200

    minor

--- models/article.js
+++ models/article.js
@@ -4,7 +4,12 @@ const Schema = mongoose.Schema;
 const ArticleSchema = new Schema({
 	value: String,
 	price: Number,
-	image: String
+	description: String,
+	image: {
+		type: Schema.Types.ObjectId,
+		ref: 'image',
+		default: null
+	}
 });
 
 const Article = mongoose.model('article', ArticleSchema, 'ARTICLE');
