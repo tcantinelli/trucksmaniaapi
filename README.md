0730a90590a85ec32c8f6ad7db6a3c6cf3e00669
Tom <tcantinelli@gmail.com>
2019-06-04 23:51:36 +0200

    ReadMe et addCategories

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-apisoapandsoftjs
+# TrucksMania API
+Serveur de démonstration
+
+API d'identification et d'accès aux données de la base de données MongoDB des applications TrucksMania
+
+##Installation
+npm install
+
+##MongoDB
+Les données des catégories sont disponibles dans le fichier addCategories.js
---
+++ addCategories.js
@@ -0,0 +1,76 @@
+{
+    "value": "africain",
+    "image": "africa.png"
+}
+{
+    "value": "asiatique",
+    "image": "rice.png"
+}
+{
+    "value": "bagel",
+    "image": "bagel.png"
+}
+{
+    "value": "bar à jus",
+    "image": "fresh.png"
+}
+{
+    "value": "pâtisserie",
+    "image": "cupcake.png"
+}
+{
+    "value": "burger",
+    "image": "burger.png"
+}
+{
+    "value": "café",
+    "image": "coffee.png"
+}
+{
+    "value": "crêpe galette",
+    "image": "crepe.png"
+}
+{
+    "value": "cuisine du monde",
+    "image": "world.png"
+}
+{
+    "value": "cuisine française",
+    "image": "france.png"
+}
+{
+    "value": "cuisine orientale",
+    "image": "samosa.png"
+}
+{
+    "value": "fruits de mer",
+    "image": "animals.png"
+}
+{
+    "value": "indien",
+    "image": "indian.png"
+}
+{
+    "value": "italien",
+    "image": "italy.png"
+}
+{
+    "value": "kebab",
+    "image": "kebab.png"
+}
+{
+    "value": "tex-mex",
+    "image": "taco.png"
+}
+{
+    "value": "pizza",
+    "image": "pizza.png"
+}
+{
+    "value": "sandwichs",
+    "image": "sandwich.png"
+}
+{
+    "value": "divers",
+    "image": "divers.png"
+}
